a35243d048029b76f119d6c1e4ca9fce55a7f990
Reemika Subrata Das <das.reemika@gmail.com>
2026-07-05 23:17:43 -0700

    Fix production module initialization

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-  { ignores: ['dist/**', 'node_modules/**', 'public/vendor/**'] },
+  { ignores: ['dist/**', 'node_modules/**', 'public/vendor/**', 'src/vendor/**'] },
   js.configs.recommended,
   {
     files: ['**/*.js'],
